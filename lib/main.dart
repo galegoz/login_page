37719af7c7b9f16d6c4d2a014b3db95074aa0a4a
galegoz <llucassduarte50@gmail.com>
2021-01-14 20:57:36 -0300

    Configurações layout e teclado

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,87 @@ class _LocationAppState extends State<LocationApp> {
           maxWidth: MediaQuery.of(context).size.width,
         ),
         decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Colors.blue[800],
-          Colors.blue[600],
-        ])),
+            gradient: LinearGradient(
+          colors: [
+            Colors.blue[800],
+            Colors.blue[600],
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.centerRight,
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 36.0, horizontal: 24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46.0,
+                          fontWeight: FontWeight.w800),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Enter to a beautifull word ",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w300),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    )),
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFe7edeb),
+                          hintText: "E-mail",
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
