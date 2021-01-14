6e14d1744b1c4c81b0bd1eab259b59578b57a5f6
galegoz <llucassduarte50@gmail.com>
2021-01-13 23:27:38 -0300

    Box decoration

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,23 @@ class LocationApp extends StatefulWidget {
   @override
   _LocationAppState createState() => _LocationAppState();
 }
+
+class _LocationAppState extends State<LocationApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+        constraints: BoxConstraints(
+          maxHeight: MediaQuery.of(context).size.height,
+          maxWidth: MediaQuery.of(context).size.width,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Colors.blue[800],
+          Colors.blue[600],
+        ])),
+      ),
+    ));
+  }
+}
