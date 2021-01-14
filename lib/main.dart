80abce46286271844d752f84532d8d7310faa5ed
galegoz <llucassduarte50@gmail.com>
2021-01-13 23:05:08 -0300

    Iniciando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LocationApp(),
+    );
+  }
+}
+
+class LocationApp extends StatefulWidget {
+  @override
+  _LocationAppState createState() => _LocationAppState();
+}
